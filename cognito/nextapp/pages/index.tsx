2f14ca430fc298770871903853555775107fdeb4
kiho.choi <kiho.choi@lge.com>
2022-06-19 03:08:35 +0900

    Update cognito test react

--- cognito/nextapp/pages/index.tsx
+++ cognito/nextapp/pages/index.tsx
@@ -4,7 +4,8 @@ import LoginForm from '../components/LoginForm';
 import styled from 'styled-components';
 import SignupForm from '../components/SignupForm';
 import userPool from '../src/userPool';
-import { CognitoUser } from 'amazon-cognito-identity-js';
+import { CognitoUser, GlobalSignOutRequest } from 'amazon-cognito-identity-js';
+// import { Auth } from 'aws-amplify';
 
 const cognitoLoginUrl = 'https://redcapmobility.auth.ap-northeast-2.amazoncognito.com';
 const Container = styled.div`
@@ -40,6 +41,30 @@ const Container = styled.div`
     }
 `;
 
+const printData = (currentUser) => {
+	currentUser.getSession((err, data) => {
+		if(data) {
+			console.log(data);
+			console.log(
+					'idToken: ', data.getIdToken()?.getJwtToken());
+			console.log(
+					'AccessToken: ', data.getAccessToken()?.getJwtToken());
+			console.log(
+					'auth_time :',
+					new Date(data.getAccessToken().payload.auth_time * 1000)
+					);
+			console.log(
+					'exp :',
+					new Date(data.getAccessToken().payload.exp * 1000)
+					);
+			console.log(
+					'iat :',
+					new Date(data.getAccessToken().payload.iat * 1000)
+					);
+		}
+	})
+}
+
 const Index: NextPage = () => {
     const [signupCheck, setSignupCheck] = useState(true);
     const [userId, setUserId] = useState<string | null>(null);
@@ -62,34 +87,137 @@ const Index: NextPage = () => {
         if (currentUser) {
             const { username } = currentUser;
 			console.log('index:', username);
-            setUserId(username);
+			printData(currentUser);
+			currentUser.getSession(function(err, data) {
+				if (err) {
+					// Prompt the user to reauthenticate by hand...
+				} else {
+					const cognitoUserSession = data;
+					const yourIdToken = cognitoUserSession.getIdToken().jwtToken;
+					const yourAccessToken = cognitoUserSession.getAccessToken().jwtToken;
+					console.log('AccessToken: ', yourAccessToken);
+					console.log('Session: ', data);
+					setSession(data);
+				}
+			});
+			setUserId(username);
         }
     }, []);
 
+	const refreshToken = () => {
+		console.log('userId: param', userId);
+		console.log('session: param', session);
+	};
+    //     const currentUser = userPool.getCurrentUser();
+	//     console.log('currentUser param: ', currentUser);
+	//     console.log('userId: param', userId);
+	//     console.log('token: param', token);
+	//     console.log('session: param', session);
+	//     printData(currentUser);
+	//     switch(1) {
+	//     case 0: {
+	//         console.log('=============== signOut =============');
+	//         currentUser?.signOut(() => {
+	//                 setUserId(null);
+	//         });
+	//         break;
+	//     }
+	//     case 1: {
+	//         // currentUser.setSignInUserSession(session) // This one raise the Exception: TypeError: Cannot read property 'getToken' of undefined
+	//         console.log('=============== globalSignOut =============');
+	//         console.log('currentUser after set session: ', currentUser);
+	//         currentUser.refreshSession(refresh{
+	//             onSuccess: function(result) {
+	//                 console.log(result);
+	//                 setUserId(null);
+	//             },
+	//             onFailure: function(err) {
+	//                 console.log(err)
+	//                 setUserId(null);
+	//             }
+	//         })
+	//         break;
+    //
+	//     }
+	//     case 2: {
+	//         // currentUser.setSignInUserSession(session) // This one raise the Exception: TypeError: Cannot read property 'getToken' of undefined
+	//         console.log('=============== revoke =============');
+	//         console.log('currentUser after set session: ', currentUser);
+	//         currentUser.revoke({
+	//             onSuccess: function(result) {
+	//                 console.log(result);
+	//                 setUserId(null);
+	//             },
+	//             onFailure: function(err) {
+	//                 console.log(err)
+	//                 setUserId(null);
+	//             }
+	//         })
+	//         break;
+	//     }
+	//     }
+	//     // console.log('token:', token);
+	//     // const param = {
+	//     //     method: "POST",
+	//     //     headers: {'Authorization': `Basic ${token}`, "Content-type": "application/x-www-form-urlencoded"},
+	//     //     body: JSON.stringify({
+	//     //         "client_id": "4nd20k5krb4pfmpml04iac9od2",
+	//     //     })
+	//     // }
+	//     // console.log('param: ', param);
+	//     // fetch(`${cognitoLoginUrl}/oauth2/revoke`, param}).then((res) => console.log(res));
+    //
+    // }, []);
+
     const logOut = useCallback(() => {
         const currentUser = userPool.getCurrentUser();
-		console.log('currentUser: ', currentUser);
-		console.log('userId:', session);
-		console.log('token:', session);
-		console.log('session:', session);
-		if(1) {
+		console.log('currentUser param: ', currentUser);
+		console.log('userId: param', userId);
+		console.log('token: param', token);
+		console.log('session: param', session);
+		printData(currentUser);
+		switch(1) {
+		case 0: {
+			console.log('=============== signOut =============');
 			currentUser?.signOut(() => {
 					setUserId(null);
 			});
-		} else {
-			currentUser.setSignInUserSession(session) // This one raise the Exception: TypeError: Cannot read property 'getToken' of undefined
+			break;
+		}
+		case 1: {
+			// currentUser.setSignInUserSession(session) // This one raise the Exception: TypeError: Cannot read property 'getToken' of undefined
+			console.log('=============== globalSignOut =============');
 			console.log('currentUser after set session: ', currentUser);
 			currentUser.globalSignOut({
-				onSuccess: function(result) {console.log(result)},
-				onFailure: function(err) {console.log(err)}
+				onSuccess: function(result) {
+					console.log(result);
+					setUserId(null);
+				},
+				onFailure: function(err) {
+					console.log(err)
+					setUserId(null);
+				}
 			})
-		}
-        // currentUser.globalSignOut(token, function(err, data) {
-		//         if (err) console.log(err, err.stack); // an error occurred
-		//         else     console.log(data);           // successful response
-		//         setUserId(null);
-        // });
+			break;
 
+		}
+		case 2: {
+			// currentUser.setSignInUserSession(session) // This one raise the Exception: TypeError: Cannot read property 'getToken' of undefined
+			console.log('=============== revoke =============');
+			console.log('currentUser after set session: ', currentUser);
+			currentUser.revoke({
+				onSuccess: function(result) {
+					console.log(result);
+					setUserId(null);
+				},
+				onFailure: function(err) {
+					console.log(err)
+					setUserId(null);
+				}
+			})
+			break;
+		}
+		}
 		// console.log('token:', token);
 		// const param = {
 		//     method: "POST",
@@ -112,6 +240,7 @@ const Index: NextPage = () => {
                         로그인 완료
                     </h3>
                     <button onClick={logOut}>로그아웃</button>
+                    <button onClick={refreshToken}>RefreshToken</button>
                 </div>
             ) : (
                 <>{signupCheck ? <LoginForm goSignup={turnOver} setUserId={setUserId} setToken={setToken} setSession={setSession}/> : <SignupForm goLogin={turnOver} />}</>
